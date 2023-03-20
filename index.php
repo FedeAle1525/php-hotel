7a250bcc1a09a051521cfb6afe79d084f8c24b25
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-20 15:22:11 +0100

    PHP print data on page with lists

--- index.php
+++ index.php
@@ -58,6 +58,39 @@ $hotels = [
 
 <body>
 
+
+  <?php
+
+  // Stampo in pagina ogni Array di Hotel
+  foreach ($hotels as $hotel) {
+  ?>
+    <ul>
+      <?php
+
+      foreach ($hotel as $key => $val) {
+      ?>
+
+        <li>
+          <span>
+            <strong><?php echo $key ?>: </strong>
+          </span>
+          <span>
+            <?php echo $val ?>
+          </span>
+        </li>
+
+      <?php
+      };
+
+      ?>
+
+    </ul>
+
+  <?php
+  };
+  ?>
+
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
